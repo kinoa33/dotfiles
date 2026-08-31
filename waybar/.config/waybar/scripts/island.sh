922b82367549dfd011318ec69edb3473fa48cdd1
kino <tnfs24103@gmail.com>
2026-08-31 14:59:14 +0300

    Delete waybar/.config/waybar/scripts directory

--- waybar/.config/waybar/scripts/island.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-mem=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100.0}')
-time=$(date +"%H:%M")
-
-echo "{\"cpu\": \"$cpu\", \"memory\": \"$mem\", \"time\": \"$time\"}"
